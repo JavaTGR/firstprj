200ac428caf68475ee36dd70758e3a17e7209a8d
JavaTGR <102856316+JavaTGR@users.noreply.github.com>
2022-04-02 15:59:05 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        First_Easy first = new First_Easy();
+        System.out.println(first.first_meth(37, 8));
+        System.out.println(first.second_meth(15));
+        System.out.println(first.third_meth(5));
+        String str ="Java";
+        int [] tst_var_1 = {-1,-3,-5,8};
+        System.out.println(first.fourth_meth(-1,tst_var_1));
+        System.out.println(first.fifth_method(tst_var_1));
+        System.out.println(first.sixth_method(str));
+
+
+
+
+    }
+}
